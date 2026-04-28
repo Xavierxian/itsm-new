173bdce068d6a4327db3e51218a80022253efdcb
xianxin <1418436581@qq.com>
2026-04-28 12:06:20 +0800

    fix: resolve favicon.ico 404 by adding favicon link and route

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 handler403 = "core.error_views.permission_denied_view"
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=f"/{settings.STATIC_URL}images/title1.png", permanent=True)),
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("accounts/", include("accounts.urls")),
